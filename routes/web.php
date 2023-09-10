c3a83607988b0ca90307cc3069c4653fae74bdbc
fhmaisonneuve <e2296133@cmaisonnuve.qc.ca>
2023-09-10 16:10:20 -0400

    preparation du projet

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get( '/blog', [EtudiantController::class, 'index'])->name('etudiant.index');
+Route::get( '/blog/{blogPost}', [EtudiantController::class, 'show'])->name('etudiant.show');
+Route::get( '/blog-create', [EtudiantController::class,'create'])->name('etudiant.create');
+Route::post( '/blog-create', [EtudiantController::class,'store']);
+Route::get( '/blog-edit/{blogPost}', [EtudiantController::class, 'edit'])->name('etudiant.edit');
+Route::put( '/blog-edit/{blogPost}', [EtudiantController::class, 'update']);
+Route::delete( '/blog-edit/{blogPost}', [EtudiantController::class, 'destroy'])->name('etudiant.delete');;
+
+Route::get('/query', [BlogPostController::class, 'query' ]);
